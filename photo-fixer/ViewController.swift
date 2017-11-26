891a6e8a98eea2f7c04060deb3d1bcd693174444
Julian Bleecker <bleeckerj@gmail.com>
2017-11-25 16:38:24 -0800

    Initial Commit

---
+++ photo-fixer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  photo-fixer
+//
+//  Created by Julian Bleecker on 11/25/17.
+//  Copyright © 2017 Near Future Laboratory. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
